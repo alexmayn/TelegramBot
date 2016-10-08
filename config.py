8a352cdec13427296da8685a2ee32da70330bbdf
Alexander maynagashev <amaynagashev@ptsecurity.com>
2016-10-08 22:50:36 +0700

    Config changes

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 token = ''
+MESSAGES_FOLDER = "D:\PyCharm\TelegramBot\TelegramBot"
+FILE_NAME = 'ministers.msg'
+
+BOT_CHAT_ID = -1001072537056
+MINIST_GROUP_CHAT_ID = -146632557
+TIMEOUT = 3400
