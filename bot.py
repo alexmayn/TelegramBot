84f201fc3154c04d76ee4c68ffa6707bcc8f38b0
Alexander maynagashev <amaynagashev@ptsecurity.com>
2016-10-08 22:49:36 +0700

    Changed sending e-mail
    Send by timer

--- bot.py
+++ bot.py
@@ -1,50 +1,130 @@
 # -*- coding: utf-8 -*-
+"""
+  Based on pyTelegramBotyAPI
+  GitHub: https://github.com/eternnoir/pyTelegramBotAPI
+  documentation: https://pypi.python.org/pypi/pyTelegramBotAPI/0.3.6
+  deploeing: https://retifrav.github.io/blog/2015/10/24/telegram-bot/
+             https://kondra007.gitbooks.io/telegram-bot-lessons/content/multiple_bots_1.html
+
+"""
+
 import config
+import time
+from datetime import datetime, timedelta
 import telebot
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import os
 
 
-fromaddr = 'Mr. Robot <aam@niipp.ru>'
+"""
+tomskchrist.tihik@gmail.com
+ALJEg#Ysn5&hGRFv#ozh7S&S9cav@z6F
+"""
 
-toaddr = 'Administrator <aam@niipp.ru>'
+fromaddr = 'Tihik Angel <tomskchrist.tihik@gmail.com>'
+toaddr =   'Administrator <maynagashev@gmail.com>'
+subj =     'Message from: Ministers Telegram group'
 
-subj = 'Notification from system'
-
-username = 'my@email.com'
-
-password = 'my_password'
+username = 'tomskchrist.tihik@gmail.com'
+password = 'ALJEg#Ysn5&hGRFv#ozh7S&S9cav@z6F'
+filePath = os.path.join(config.MESSAGES_FOLDER, config.FILE_NAME)
 
 server = smtplib.SMTP('smtp.gmail.com:587')
-
-server.set_debuglevel(1);
-
+server.ehlo()
+server.set_debuglevel(1)
 
 bot = telebot.TeleBot(config.token)
 
+messagesToSend = []
+# getMe
+user = bot.get_me()
+print(user)
+
+# getUpdates
+updates = bot.get_updates(1234, 100, 20) #get_Updates(offset, limit, timeout):
+print(updates)
+
+
+
+# Run smtp server
 server.starttls()
 server.login(username, password)
 
-@bot.message_handler(content_types=["text"])
+start = datetime.now()
+
+def listener(messages):
+    for m in messages:
+        print(str(m))
+
+
+def sendMessages():
+    '''
+    Load all messages from file? read and send to e-mail
+    :param message:
+    :return:
+    '''
+
+    messagesToSend = []
+    with open(filePath, mode='r', encoding="utf8") as messgeFile:
+         for line in messgeFile:
+             messagesToSend.append(line)
+
+    messagesToSend = ''.join(messagesToSend)
+
+    msg = MIMEMultipart("alternative")
+    msg["Subject"] = subj
+    msg["From"] = fromaddr
+    msg["To"] = toaddr
+    text = "".join(messagesToSend)
+    part1 = MIMEText(text.encode("utf-8"), "plain", "utf-8")
+    msg.attach(part1)
+    server.sendmail(fromaddr, toaddr, msg.as_string().encode('ascii'))
+
+@bot.message_handler(content_types=['document', 'audio'])
+def handle_docs_audio(message):
+    """
+    This func can be get media data from thred and send it by e-mail
+    """
+    pass
+
+@bot.message_handler(func=lambda m: True, content_types=['text'])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, 'This is my response for you!')
+    global messagesToSend
+    global timeout
+    global start
 
-    if message.text:
+    # This is filter by group Id
+    if message.text and (message.chat.id == config.MINIST_GROUP_CHAT_ID or message.chat.id == config.BOT_CHAT_ID):
+        if message.from_user.first_name != None:
+            authorName = message.from_user.first_name
+        else:
+            authorName = ''
 
-        msg = MIMEMultipart("alternative")
-        msg["Subject"] = subj
+        if message.from_user.last_name != None:
+            authorName = '{0} {1}'.format(authorName, message.from_user.last_name)
+        else:
+            authorName
+        #messageTime = message.date #datetime.fromtimestamp(message.date).strftime('%Y %m %d  %H:%M:%S')
+        text = "\n{0} - {1}: \n {2}".format('Date:', authorName, message.text)
 
-        msg["From"] = fromaddr
-        msg["To"] = toaddr
+        '''
+         Save all messages to file
+        '''
+        with open(filePath, mode='a', encoding="utf8") as messgeFile:
+            messgeFile.write(text)
+            messgeFile.close()
 
-        part1 = MIMEText(message.text.encode("utf-8"), "plain", "utf-8")
-        msg.attach(part1)
+        '''
+          Send all messages afte timer is finished
+        '''
+        if datetime.now() >= start + timedelta(seconds=config.TIMEOUT):
+            sendMessages()
+            os.remove(filePath)
+            start = datetime.now()
 
-        server.sendmail(fromaddr, toaddr, msg.as_string().encode('ascii')) #
-
-        bot.send_message(message.chat.id, 'Sended to e-mail')
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+     bot.polling(none_stop=True, interval=0)
 
